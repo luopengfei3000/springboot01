3a2e293df9dc86c2dc9a3047da8996d815a26699
luopengfei3000 <luopengfei3000@163.com>
2018-09-20 15:27:13 +0800

    banner控制台显示在启动类中设置

--- src/main/java/com/example/springboot01/Springboot01Application.java
+++ src/main/java/com/example/springboot01/Springboot01Application.java
@@ -1,13 +1,28 @@
 package com.example.springboot01;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Springboot01Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot01Application.class, args);
+        // 启动方式一：
+        //SpringApplication.run(Springboot01Application.class, args);
+
+        // 启动方式二：
+//        SpringApplication springApplication = new SpringApplication(Springboot01Application.class);
+//        springApplication.setBannerMode(Banner.Mode.OFF);
+//        springApplication.run(args);
+
+        // 启动方式三：
+        new SpringApplicationBuilder(Springboot01Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .build()
+                .run(args);
+
         //效果和MyStartupRunnerTest2一样
         /**
          System.out.println("=================================");
